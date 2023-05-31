6abf2ff7dc7dd0e18016e128d6ca70624e7b53f2
Yasu <Yasu@DESKTOP-FUOOUNG>
2023-05-31 03:31:27 +0300

    test

--- PersonnelManagement/Model/Position.cs
+++ PersonnelManagement/Model/Position.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//TODO добавить поля количества сотрудников на должности и бюджет на всех сотрудников в одной должности
 
 namespace PersonnelManagement.Model
 {    
